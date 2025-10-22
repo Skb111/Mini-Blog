e6c785690d6d7ac8114c640e1317ff69608a348d
Sadiq Kabir <webemail655@gmail.com>
2025-10-22 13:22:47 +0100

    Blog Routes with api

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,11 @@ import swaggerSpec from './config/swagger';
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes';
+import blogRoutes from './routes/blogRoutes';
 import { connectDB } from './config/database';
 import morgan from 'morgan';  
 import cors from 'cors';
-import blogRoutes from './routes/blogRoutes';
 
-dotenv.config();
 dotenv.config();
 
 console.log("Loaded Mongo URI:", process.env['MONGODB_URI']);
@@ -27,7 +26,7 @@ app.get('/', (req, res) => {
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use("/api/auth", authRoutes);
-app.use("api/blogs", blogRoutes)
+app.use("/api/blogs", blogRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
